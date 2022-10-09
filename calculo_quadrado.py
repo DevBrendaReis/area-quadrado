71a5aad9d23796c234fe6dfebd3225384b4b4698
DevBrendaReis <dev.brendareis@gmail.com>
2022-10-09 18:10:10 -0300

    first commit

---
+++ calculo_quadrado.py
@@ -0,0 +1,5 @@
+lado = float(input('Informe o comprimento lateral do quadrado: '))
+area = lado**2
+dobro_da_area = area * 2 
+print()
+print(f'O dobro da área deste quadrado é {dobro_da_area:.2f}')
